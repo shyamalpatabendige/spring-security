34f481b810079aa8d3904387481d19762370c122
shyamalmadura <{username}@users.noreply.github.com>
2023-07-09 15:49:28 +1000

    Retrieve user details from oauth2 login

--- src/main/java/com/shyamalmadura/spring/spring/security/controller/WebController.java
+++ src/main/java/com/shyamalmadura/spring/spring/security/controller/WebController.java
@@ -1,7 +1,11 @@
 package com.shyamalmadura.spring.spring.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class WebController {
@@ -12,7 +16,17 @@ public class WebController {
     }
 
     @GetMapping("/secured")
-    public String securedPage() {
-        return "Hello, you are allowed to access this page";
+    public String securedPage(Authentication authentication) {
+        return "Hello [" +
+                getName(authentication)
+                + "], you are allowed to access this page";
+    }
+
+    private String getName(Authentication authentication) {
+        return Optional.of(authentication.getPrincipal())
+                .filter(OidcUser.class::isInstance)
+                .map(OidcUser.class::cast)
+                .map(OidcUser::getEmail) //Can be extended to get user details as well
+                .orElseGet(authentication::getName);
     }
 }
