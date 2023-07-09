e19f7dcf669b53220ce23193ece5d697068575c0
shyamalmadura <{username}@users.noreply.github.com>
2023-07-09 15:05:46 +1000

    Adding Form login
    Still the User credentials are missing thus unable to login

--- src/main/java/com/shyamalmadura/spring/spring/security/config/SecurityConfig.java
+++ src/main/java/com/shyamalmadura/spring/spring/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.shyamalmadura.spring.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +18,7 @@ public class SecurityConfig {
                     authConfig.antMatchers("/secured").authenticated();
                     authConfig.anyRequest().permitAll();
                 })
+                .formLogin(Customizer.withDefaults())
                 .build();
     }
 }
