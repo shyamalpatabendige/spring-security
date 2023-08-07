b1bdac1c24099b2002cd8f096aaa9c7b6c2050e7
shyamal <{username}@users.noreply.github.com>
2023-08-07 16:58:04 +1000

    Add a custom filter

---
+++ src/main/java/com/shyamalmadura/spring/spring/security/filter/CustomFilter.java
@@ -0,0 +1,17 @@
+package com.shyamalmadura.spring.spring.security.filter;
+
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("Hello from the CUSTOM filter..");
+    }
+}
